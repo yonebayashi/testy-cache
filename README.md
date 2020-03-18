b97b3a023ffd2e6d0e76039464fa865c2e833c60
david tamas-parris <davidtp@reed.edu>
2020-03-18 06:07:29 -0700

    new changes, we should see if the tests pass on mac

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ g++ -std=c++17 -Wall -o test_cache_lib test_cache_lib.cc && ./test_cache_lib --s
 ```
 ## Part 1
 
-We test the following cases for when the Evictor is a nullptr Evictor and when it is a FIFO evictor.
+We test the following cases for when the Evictor is a nullptr Evictor:
 
-Test name | Description | Status
---- | --- | ---
+Test name/ Description | Status
+ --- | --- | ---
 Set/Get | Getting a key that wasn't inserted | PASSED
 Set/Get | Getting a key that was inserted | PASSED
 Set/Get | Getting a key that was inserted and modified | PASSED
@@ -21,13 +21,15 @@ Set/Get | Getting a key that was inserted and deleted | PASSED
 Space used | Memory used is initially empty and should be updated correctly when new keys are inserted or removed | PASSED
 Reset | Calling `reset` returns an empty cache | PASSED
 
+It should be noted that these tests were compiled and run on a Mac. When compiled in the virtual machine, the tests stop due to an instance of `std::length_error`. After running our original tests in valgrind, we believe this is due to a memory error.
+
 ## Part 2
 
 ### Aaron/Alex
 
 [Project repo](https://gitlab.com/InternetUnexplorer/CSCI_389_HW2)
 
-No compilation errors.
+We ran into a small compilation error. Adding `#pragma once` to `fifo_evictor.hh` fixed it. Most tests pass, but several tests fail due to incorrect values for `size` after running get. Additonally, the evictor fails the "FIFO evictor displays FIFO functionality with nonunique keys" and "FIFO evictor displays FIFO functionality with unique keys" tests.
 
 Test name | Status
 --- | ---
@@ -45,9 +47,13 @@ Reset | PASSED
 Test name | Status
 --- | ---
 
+The tests hault due to a segmentation fault when attempting to test the FIFO evictor by repeatedly touching keys. Otherwise, the only assertions that fail have to do with incorrect values for `size` after retreiving a key.
 ### Aryeh/Connor
 
 [Project repo](https://github.com/astah100/HW2AryehStahlAndConnorDeiparine)
 
+
 Test name | Status
 --- | ---
+
+This group seems to have modified the header file and used a nonstandard interface, causing compilation issues. They can be fixed by renaming their FIFO evictor class from `FIFO` to `FifoEvictor`. Tests halt on this code due to a SIGABORT on the "Cache should handle 100 insertions without crashing" test.
--- test_cache_lib.cc
+++ test_cache_lib.cc
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <typeinfo>
 #include <cstring>
+#include <stdlib.h>
 
 #include "evictor.hh"
 #include "cache.hh"
@@ -28,40 +29,90 @@ TEST_CASE ("Cache with nullptr Evictor", "[cache]") {
 
   Cache::size_type size;
 
-  SECTION( "should not get a key that wasn't inserted" ) {
+  SECTION( "get should not retreive a key that wasn't inserted" ) {
     REQUIRE(cache.get("k1", size) == NULL);
     REQUIRE(size == 0);
   }
 
-  SECTION( "should get a key that was inserted" ) {
+  SECTION ("Cache should handle 100 insertions without crashing") {
+    for (unsigned i = 0 ; i<100; i++) {
+      cache.set({(char)(i+1), '\0'},val1, size);
+    }
+  }
+
+  SECTION ("Cache should handle 100 deletions without crashing") {
+    for (unsigned i = 0 ; i<100; i++) {
+      cache.set({(char)(i+1), '\0'}, val1, size);
+    }
+    for (unsigned i = 0 ; i<100; i++) {
+      cache.del({(char)(i+1), '\0'});
+    }
+  }
+
+  SECTION( "get should retreive a key that was inserted (with 1 item in cache)" ) {
     cache.set("k1", val1, strlen(val1)+1);
 
     REQUIRE(strcmp(cache.get("k1", size), val1) == 0);   // string comparision
     REQUIRE(size == strlen(val1)+1);
 
-    // inserting extra keys should increment memory used correctly
+  }
+
+    SECTION( "get should retreive a key that was inserted (with 2 items in cache)" ) {
+    cache.set("k1", val1, strlen(val1)+1);
     cache.set("k2", val2, strlen(val2)+1);
 
     REQUIRE(strcmp(cache.get("k2", size), val2) == 0);
     REQUIRE(size == strlen(val2)+1);
-    REQUIRE(cache.space_used() == 4);
 
-    // should not insert any more key when maxmem has been exceeded
+  }
+
+  SECTION( "get should retreive a key that was inserted (with 3 items in cache)" ) {
+    cache.set("k1", val1, strlen(val1)+1);
+
+    cache.set("k2", val2, strlen(val2)+1);
+
+
     cache.set("k3", val3, strlen(val3)+1);
 
     REQUIRE(strcmp(cache.get("k3", size), val3) == 0);
     REQUIRE(size == strlen(val3)+1);
     REQUIRE(cache.space_used() == 7);
 
+
+  }
+
+  SECTION("space_used() responds appropriately to new insertions") {
+    cache.set("k1", val1, strlen(val1)+1);
+    REQUIRE(cache.space_used()==2);
+
+    cache.set("k2", val2, strlen(val2)+1);
+    REQUIRE(cache.space_used()==4);
+
+    cache.set("k3", val3, strlen(val3)+1);
+    REQUIRE(cache.space_used()==7);
+
+  }
+
+  SECTION ("Eviction policy with nullptr evictor should not allow an insertion that would cause an overflow.") {
+    cache.set("k1", val1, strlen(val1)+1);
+
+    cache.set("k2", val2, strlen(val2)+1);
+
+
+    cache.set("k3", val3, strlen(val3)+1);
+
+
+    REQUIRE(cache.space_used()==7);
+
     cache.set("k4", val4, strlen(val4)+1);
 
     REQUIRE(cache.get("k4", size) == NULL);
     REQUIRE(size == 0);
     REQUIRE(cache.space_used() == 7);
-    cache.reset();
   }
 
-  SECTION( "should get a key that was inserted and modified" ) {
+
+  SECTION( "get should retreive a key that was inserted and modified" ) {
     cache.set("k3", val3, strlen(val3)+1);
 
     REQUIRE(strcmp(cache.get("k3", size), val3) == 0);
@@ -74,10 +125,9 @@ TEST_CASE ("Cache with nullptr Evictor", "[cache]") {
     REQUIRE(strcmp(cache.get("k3", size), val4) == 0);
     REQUIRE(size == strlen(val4)+1);
     REQUIRE(cache.space_used() == strlen(val4)+1);
-    cache.reset();
   }
 
-  SECTION( "should not get a key that was inserted and deleted" ) {
+  SECTION( "get should not retreive a key that was inserted and deleted" ) {
     cache.set("k1", val1, strlen(val1)+1);
 
     REQUIRE(strcmp(cache.get("k1", size), val1) == 0);   // string comparision
@@ -88,7 +138,6 @@ TEST_CASE ("Cache with nullptr Evictor", "[cache]") {
     REQUIRE(cache.get("k1", size) == NULL);
     REQUIRE(size == 0);
     REQUIRE(cache.space_used() == 0);
-    cache.reset();
   }
 
   SECTION( "resetting should return empty cache" ) {
@@ -97,7 +146,8 @@ TEST_CASE ("Cache with nullptr Evictor", "[cache]") {
 
     cache.reset();
     REQUIRE(cache.space_used() == 0);
-    cache.reset();
+    REQUIRE(cache.get("k1", size)== NULL);
+    REQUIRE(cache.get("k2", size)== NULL);
   }
 }
 
@@ -114,35 +164,117 @@ TEST_CASE ("Cache with FIFO Evictor", "[evictor]") {
 
   Cache::size_type size;
 
-  SECTION( "should not get a key that wasn't inserted" ) {
-    REQUIRE(cache.get("k1", size) == NULL);
-    REQUIRE(size == 0);
-    cache.reset();
+  SECTION ("FIFO evictor can handle 100 key touches without crashing") {
+    for (unsigned i = 1 ; i<101; i++) {
+      evictor->touch_key({(char)(i), '\0'});
+    }
   }
 
-  SECTION( "should get a key that was inserted" ) {
+  SECTION ("FIFO evictor displays FIFO functionality with unique keys") {
+    for (unsigned i = 1 ; i<101; i++) {
+      evictor->touch_key({(char)(i), '\0'});
+    }
+    char expected_str[] ="X";
+    for (unsigned i=100; i>0; i--) {
+      expected_str[0]= (char)i;
+      REQUIRE(evictor-> evict()==expected_str);
+    }
+  }
+
+  SECTION ("FIFO evictor displays FIFO functionality with nonunique keys") {
+
+    for (unsigned i = 1 ; i<101; i++) {
+      evictor->touch_key({(char)(i), '\0'});
+    }
+    for (unsigned i = 1 ; i<101; i++) {
+      evictor->touch_key({(char)(i), '\0'});
+    }
+    char expected_str[] ="X";
+    for (unsigned i=100; i>0; i--) {
+      expected_str[0]= (char)i;
+      REQUIRE(evictor-> evict()==expected_str);
+    }
+    for (unsigned i=100; i>0; i--) {
+      expected_str[0]= (char)i;
+      REQUIRE(evictor-> evict()==expected_str);
+    }
+  }
+
+
+  SECTION( "get should not retreive a key that wasn't inserted" ) {
+    REQUIRE(cache.get("k1", size) == NULL);
+    REQUIRE(size == 0);
+  }
+
+  SECTION ("Cache should handle 100 insertions without crashing") {
+    for (unsigned i = 0 ; i<100; i++) {
+      cache.set({(char)(i+1), '\0'},val1, size);
+    }
+    REQUIRE(size == 2);
+  }
+
+  SECTION ("Cache should handle 100 deletions without crashing") {
+    for (unsigned i = 0 ; i<100; i++) {
+      cache.set({(char)(i+1), '\0'}, val1, size);
+    }
+    for (unsigned i = 0 ; i<100; i++) {
+      cache.del({(char)(i+1), '\0'});
+    }
+  }
+  SECTION( "get should retreive a key that was inserted (with 1 item in cache)" ) {
     cache.set("k1", val1, strlen(val1)+1);
 
     REQUIRE(strcmp(cache.get("k1", size), val1) == 0);   // string comparision
     REQUIRE(size == strlen(val1)+1);
 
-    // inserting extra keys should increment memory used correctly
+  }
+
+    SECTION( "get should retreive a key that was inserted (with 2 items in cache)" ) {
+    cache.set("k1", val1, strlen(val1)+1);
     cache.set("k2", val2, strlen(val2)+1);
 
     REQUIRE(strcmp(cache.get("k2", size), val2) == 0);
     REQUIRE(size == strlen(val2)+1);
-    REQUIRE(cache.space_used() == 4);
 
-    // when maxmem has been exceeded, should evict existing keys in FIFO order to make space for the new key
+  }
+
+  SECTION( "get should retreive a key that was inserted (with 3 items in cache)" ) {
+    cache.set("k1", val1, strlen(val1)+1);
+
+    cache.set("k2", val2, strlen(val2)+1);
+
+
     cache.set("k3", val3, strlen(val3)+1);
 
     REQUIRE(strcmp(cache.get("k3", size), val3) == 0);
     REQUIRE(size == strlen(val3)+1);
     REQUIRE(cache.space_used() == 7);
 
+
+  }
+
+  SECTION("space_used() responds appropriately to new insertions") {
+    cache.set("k1", val1, strlen(val1)+1);
+    REQUIRE(cache.space_used()==2);
+
+    cache.set("k2", val2, strlen(val2)+1);
+    REQUIRE(cache.space_used()==4);
+
+    cache.set("k3", val3, strlen(val3)+1);
+    REQUIRE(cache.space_used()==7);
+
+  }
+
+  SECTION( "Inserting works with FIFO eviction" ) {
+    cache.set("k1", val1, strlen(val1)+1);
+
+    cache.set("k2", val2, strlen(val2)+1);
+
+    cache.set("k3", val3, strlen(val3)+1);
+
     cache.set("k4", val4, strlen(val4)+1);
 
-    // successfully inserted key 4
+    // Testing that k4 was successfully inserted
     REQUIRE(strcmp(cache.get("k4", size), val4) == 0);
     REQUIRE(size == strlen(val4)+1);
 
@@ -152,10 +284,9 @@ TEST_CASE ("Cache with FIFO Evictor", "[evictor]") {
 
     // total memory used is occupied by key 2->4
     REQUIRE(cache.space_used() == 8);
-    cache.reset();
   }
 
-  SECTION( "should get a key that was inserted and modified" ) {
+  SECTION( "get should retreive a key that was inserted and modified" ) {
     cache.set("k3", val3, strlen(val3)+1);
 
     REQUIRE(strcmp(cache.get("k3", size), val3) == 0);
@@ -168,10 +299,9 @@ TEST_CASE ("Cache with FIFO Evictor", "[evictor]") {
     REQUIRE(strcmp(cache.get("k3", size), val4) == 0);
     REQUIRE(size == strlen(val4)+1);
     REQUIRE(cache.space_used() == strlen(val4)+1);
-    cache.reset();
   }
 
-  SECTION( "should not get a key that was inserted and deleted" ) {
+  SECTION( "get should not retreive a key that was inserted and deleted" ) {
     cache.set("k1", val1, strlen(val1)+1);
 
     REQUIRE(strcmp(cache.get("k1", size), val1) == 0);   // string comparision
@@ -182,7 +312,6 @@ TEST_CASE ("Cache with FIFO Evictor", "[evictor]") {
     REQUIRE(cache.get("k1", size) == NULL);
     REQUIRE(size == 0);
     REQUIRE(cache.space_used() == 0);
-    cache.reset();
   }
 
   SECTION( "resetting should return empty cache" ) {
@@ -191,6 +320,8 @@ TEST_CASE ("Cache with FIFO Evictor", "[evictor]") {
 
     cache.reset();
     REQUIRE(cache.space_used() == 0);
-    cache.reset();
+    REQUIRE(cache.get("k1", size)== NULL);
+    REQUIRE(cache.get("k2", size)== NULL);
   }
+
 }
